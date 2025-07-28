7ee351e21bf5edf529659937d2b4f02d514a2224
Étienne Courchesne <etienne@troiscent.ca>
2025-07-28 09:54:24 -0400

    added enlgish version of privacy policy page

--- src/app/[locale]/privacy/page.tsx
+++ src/app/[locale]/privacy/page.tsx
@@ -2,25 +2,22 @@
 
 import { Button } from '@/components/ui/buttons/button'
 import { useRouter } from '@/i18n/navigation'
+import { useTranslations } from 'next-intl'
 
 export default function Page() {
+    const t = useTranslations()
     const router = useRouter()
 
     return (
         <div className="w-full h-screen portrait:min-h-[100dvh] md:flex items-center justify-center p-5 overflow-auto">
             <main className="w-full max-w-[450px] h-auto bg-off-white rounded-lg p-5">
-                <h1 className="text-2xl font-semibold tracking-tight mb-8">Politique de confidentialité</h1>
+                <h1 className="text-2xl font-semibold tracking-tight mb-8">{t('privacy.title')}</h1>
 
-                <p className="mb-4">Nous ne recueillons aucune information personnelle directement sur ce site.</p>
-                <p>
-                    Toutefois, lorsque vous utilisez notre outil de réservation de rendez-vous (fourni par Cal.com),
-                    certaines données personnelles peuvent être collectées par cette plateforme externe, notamment votre
-                    nom, votre courriel et votre disponibilité.
-                </p>
+                <p className="mb-4">{t('privacy.p1')}</p>
+                <p>{t('privacy.p2')}</p>
 
                 <p className="mb-4">
-                    En utilisant le système de réservation, vous acceptez la politique de confidentialité de Cal.com.
-                    Pour en savoir plus, vous pouvez consulter leur politique ici :{' '}
+                    {t('privacy.p3')}{' '}
                     <a
                         href="https://cal.com/privacy"
                         className="text-blue-600 underline"
@@ -31,17 +28,16 @@ export default function Page() {
                     </a>
                 </p>
 
-                <p className='py-4'>
-                    Pour toute question concernant vos renseignements personnels, vous pouvez contacter notre
-                    responsable de la protection des renseignements :
+                <p className="py-4">
+                    {t('privacy.p4')}
                     <br />
-                    <strong className='block pt-4'>Étienne Courchesne</strong>
+                    <strong className="block pt-4">Étienne Courchesne</strong>
                     <a href="mailto:etienne@troiscent.ca">etienne@troiscent.ca</a>
                 </p>
 
-                <p className="text-xs text-gray-500 mb-8">Dernière mise à jour : Juillet 2025</p>
+                <p className="text-xs text-gray-500 mb-8">{t('privacy.updated')}</p>
 
-                <Button variant="black" text="Close" onClick={() => router.push('/')} />
+                <Button variant="black" text={t('kw.close')} onClick={() => router.push('/')} />
             </main>
         </div>
     )
